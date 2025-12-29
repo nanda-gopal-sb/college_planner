afe8912ef83f4e0564dfc444d88703d750838d48
nanda-gopal-sb <nandagopal.nsb@gmail.com>
2025-12-30 00:54:26 +0530

    added login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new # Just an empty object to build the form
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :college)
   end
 end
